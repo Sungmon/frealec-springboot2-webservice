c7c94ac345870f1225959a0e903abed473060a68
hong <tjdals1102@daum.net>
2020-05-12 11:45:49 +0900

    2020-05-12 am 11:45

--- src/main/java/com/jojo/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojo/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.jojo.book.springboot.service.posts;
 
 import com.jojo.book.springboot.domain.posts.Posts;
 import com.jojo.book.springboot.domain.posts.PostsRepository;
+import com.jojo.book.springboot.web.dto.PostsListResponseDto;
 import com.jojo.book.springboot.web.dto.PostsResponseDto;
 import com.jojo.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojo.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -34,5 +38,18 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
